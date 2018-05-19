e0bd99c5b077c9aefd750f852e389c1e51561dea
Pascal LE BOUDIN <fxrdude@gmail.com>
2018-05-19 13:29:16 +0700

    Change client id by socket number

--- x86/LMT/Pthread/PXRHomework/src/server.cpp
+++ x86/LMT/Pthread/PXRHomework/src/server.cpp
@@ -42,7 +42,7 @@ void Server::dispatchConnection() {
   socklen_t sockSize = sizeof(sockaddr_in);
 
   while(1) {
-    cout << "Waiting new connection ... " << endl;
+    cout << "\nWaiting new connection ... " << endl;
 
     int sock = accept(m_ServerSock, (struct sockaddr *) &m_ClientAddr, &sockSize);
     if(sock < 0) {
@@ -86,12 +86,12 @@ void *Server::handleClient(void *args) {
 
   while(1) {
     memset(buffer, 0, sizeof buffer);
-    cout << "Waiting client " << client->getId() << " ..." << endl;
+    cout << "\nWaiting client " << client->getSocket() << " ..." << endl;
     n = recv(client->getSocket(), buffer, sizeof buffer, 0);
 
     //Client disconnected?
     if(n == 0) {
-      cout << "Client " << client->getId() << " diconnected" << endl;
+      cout << "Client " << client->getSocket() << " diconnected" << endl;
       close(client->getSocket());
       client->setDisconnect();
       
@@ -104,11 +104,11 @@ void *Server::handleClient(void *args) {
       cout << " ==>:"<< message << endl;
       sendToAll(message);
 
-      // Stop communication with client
+      // Stop communication with client, end thread
       return nullptr;
     }
     else if(n < 0) {
-      cerr << "Error ("<< errno  << ") while receiving message from client: " << client->getId() << endl;
+      cerr << "Error ("<< errno  << ") while receiving message from client: " << client->getSocket() << endl;
     }
     else {
       // Message received, process it
@@ -128,7 +128,7 @@ void *Server::handleClient(void *args) {
       }
 
       // Client violates the rule, close it
-      cout << "Client " << client->getId() << " violates the rule, close it" << endl;
+      cout << "Client " << client->getSocket() << " violates the rule, close it" << endl;
       close(client->getSocket());
       client->setDisconnect();
       
@@ -136,7 +136,7 @@ void *Server::handleClient(void *args) {
       removeClient(client); 
       delete client;
 
-      // Stop communication with client
+      // Stop communication with client, end thread
       return nullptr;
     }
   }
@@ -150,9 +150,13 @@ void Server::sendToClient(Client *client, char *message) {
   CustomThread::lockMutex("'sendToClient()'");
 
     if (client != nullptr && client->isConnected()) {
+      cout << "Sending to client " << client->getSocket() << endl;
       int n = send(client->getSocket(), message, strlen(message), 0);
       cout << n << " bytes sent." << endl;
     }
+    else {
+       cout << "Client is unavailable" << endl;
+    }	
 
   //Release the lock
   CustomThread::unlockMutex("'sendToClient()'");
@@ -163,34 +167,39 @@ void Server::sendToAll(char *message) {
   CustomThread::lockMutex("'sendToAll()'");
  
     for(size_t i=0; i<m_Clients.size() && m_Clients[i].isConnected(); i++) {
-      cout << "Sending to client " << m_Clients[i].getId() << endl;
+      cout << "Sending to client " << m_Clients[i].getSocket() << endl;
       int n = send(Server::m_Clients[i].getSocket(), message, strlen(message), 0);
       cout << n << " bytes sent." << endl;
     }
-   
-  //Release the lock  
+
+  //Release the lock
   CustomThread::unlockMutex("'sendToAll()'");
 }
 
+//Add client in Static clients <vector> (Critical section!)
 void Server::addClient(Client *client) {
-  //Add client in Static clients <vector> (Critical section!)
+  //Acquire the lock
   CustomThread::lockMutex("addClient()");
+
     //Before adding the new client, calculate its id. (Now we have the lock)
     client->setId(Server::m_Clients.size());
 //    cout << "Adding client with id: " << client->getId() << endl;
     Server::m_Clients.push_back(*client);
 
+  //Release the lock
   CustomThread::unlockMutex("addClient()");
 }
 
+//Remove client from Static clients <vector> (Critical section!)
 void Server::removeClient(Client *client) {
-  //Remove client in Static clients <vector> (Critical section!)
+  //Acquire the lock
   CustomThread::lockMutex("removeClient()");
 
   int index = Server::findClientIndex(client);
 //  cout << "Erasing user in position " << index << " whose name id is: "<< Server::m_Clients[index].getId() << endl;
   Server::m_Clients.erase(Server::m_Clients.begin() + index);
 
+  //Release the lock
   CustomThread::unlockMutex("removeClient()");        
 }
 
